619622f4add541cbfcabea6c3b09b84ce82f7666
lpastor74 <plaslo@gmail.com>
2021-05-21 12:40:21 -0500

    add mydb

--- init.sql
+++ init.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE flaskapi;
 USE flaskapi;
 CREATE TABLE users(user_id INT PRIMARY KEY AUTO_INCREMENT, user_name VARCHAR(255), user_email VARCHAR(255), user_password VARCHAR(255));
+USE mysql;
