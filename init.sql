f97e120199ec33ee2888cbec9031f01c5ed0d183
lpastor74 <plaslo@gmail.com>
2021-05-21 14:35:31 -0500

    add user arun with permision

--- init.sql
+++ init.sql
@@ -1,3 +1,5 @@
+CREATE USER 'arun'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON * . * TO 'arun'@'%';
 CREATE DATABASE flaskapi;
 USE flaskapi;
 CREATE TABLE users(user_id INT PRIMARY KEY AUTO_INCREMENT, user_name VARCHAR(255), user_email VARCHAR(255), user_password VARCHAR(255));
